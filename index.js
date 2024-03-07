7bb8374f2abd0e5d92643ac965175e5b8a92c9d4
HenLau0 <162377378+HenLau0@users.noreply.github.com>
2024-03-07 20:49:40 +0000

    Text fields set, working on if statements to determine URLs

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Add JavaScript below
+function submit(){
+    var MonPay = document.getElementById('MonthlyPayment').value;
+    var DowPay = document.getElementById('DownPayment').value;
+    return Dowpay*MonPay
+//     if (searchTerm === 'banana') {
+//        document.getElementById('output').innerHTML = 'Search term matched: banana';
+//    } else {
+//        document.getElementById('output').innerHTML = 'No matching result for the search term: ' + searchTerm;
+//   }
+}
